edbb3d192c90bc537ba9adfed286a3166a6a21ce
ashiller <89662655+ashiller@users.noreply.github.com>
2021-11-15 23:55:01 -0500

    Add files via upload

---
+++ src/main/java/ucf/assignments/App.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Anthony Shiller
+ */
+package ucf.assignments;
+
+public class App {
+    public static void main(String[] args) {
+
+        String name = 'Susan';
+        System.out.println(name.toLowerCase());
+    }
+}
